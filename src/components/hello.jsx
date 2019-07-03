9ab8c08e74c66ba3bf47496e40b89a041d364755
Takayuki Imagawa <imapaso16@gmail.com>
2019-07-03 14:52:04 +0900

    added state feature

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -3,16 +3,19 @@ import Hero from "./hero";
 import About from "./about";
 
 class Hello extends Component {
-  state = {};
+  state = {
+    links: ["ABOUT", "NEWS", "CONTACT"]
+  };
   render() {
+    const { links } = this.state;
     return (
       <div>
         <div className="header">
           <p>HEADER</p>
           <ul>
-            <li>ABOUT</li>
-            <li>NEWS</li>
-            <li>CONTACT</li>
+            <li>{links[0]}</li>
+            <li>{links[1]}</li>
+            <li>{links[2]}</li>
           </ul>
         </div>
         <Hero />
