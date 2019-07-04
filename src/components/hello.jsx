2781f340d7b9563668c994b19f6e958174ca5982
Takayuki Imagawa <imapaso16@gmail.com>
2019-07-04 14:22:22 +0900

    Added toggle feature

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -4,26 +4,30 @@ import About from "./about";
 import Lorem from "./lorem";
 
 class Hello extends Component {
-  state = {
-    active: "false"
-  };
-
-  handleClick = () => {
-    this.setState({
-      active: "true"
-    });
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: "false",
+      info: "Lorem Ipsum"
+    };
+  }
 
   render() {
-    const { active } = this.state;
+    const { active, info } = this.state;
     return (
       <div>
         <div className="header">
           <p>HEADER</p>
           <ul>
-            <li onClick={e => this.handleClick(e)}>ABOUT is {active}</li>
+            <li
+              onClick={() => this.setState({ active: !active })}
+              style={{ color: active ? "#ff5959" : "#111" }}
+            >
+              ABOUT
+            </li>
             <li>NEWS</li>
             <li>CONTACT</li>
+            <li>{info}</li>
           </ul>
         </div>
         <Hero />
--- src/components/lorem.jsx
+++ src/components/lorem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Lorem = () => {
+const Lorem = ({ info }) => {
   return (
     <div className="lorem-section">
-      <h1>Lorem Ipsum</h1>
+      <h1>{info}</h1>
       <svg height="100" width="100">
         <circle id="circle" cx="40" cy="55" r="10" fill="#ff5959" />
       </svg>
